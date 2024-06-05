02d74dc5ac847e81bf9012f0de95697e3909c0f7
grilldb97 <dustin.bellem@web.de>
2024-06-05 16:06:22 +0200

    Header aktualisiert

--- src/js/header-component.js
+++ src/js/header-component.js
@@ -10,48 +10,54 @@ class HeaderComponent extends HTMLElement {
   }
 
   subNav(event) {
-    const elements = this.shadowRoot.querySelectorAll('.subNav a:not(#subNavForm)');
     const subNav = this.shadowRoot.querySelector(".subNav");
-    for(let i=0; i<elements.length; i++){
-        if(elements[i].classList.contains("visibility")){
-            elements[i].classList.remove("visibility");
-            elements[i].style.background = 'linear-gradient(to bottom right, rgb(246, 246, 246), rgb(158, 158, 158))';
-            elements[i].style.boxShadow = '0px 0px 15px -4px #000000';
-        } else {
-            elements[i].classList.add("visibility");
-            elements[i].style.background = 'none';
-            elements[i].style.boxShadow = 'none';
-        }
-      }
+    const subNavElements = subNav.querySelector(".subNavElements");
+
+    if (subNavElements.style.display === "none") {
+        subNavElements.style.display = "flex";
+    } else {
+        subNavElements.style.display = "none";
+    }
+    
   }
 
   render() {
       this.shadowRoot.innerHTML = `<link rel="stylesheet" href="../css/styleHeader.css">
       <header>
           <nav>
-              <div class="navLeft">
-                  <a href="../html/index.html" class="navLogo">
+              <ul class="navLeft">
+                  <li class="navLogo">
+                  <a href="../html/index.html">
                       <img src="../images/IntraDLTransparent.png" alt="IntraDL Logo">
-                  </a>
-              </div>
+                    </a>
+                  </li>
+              </ul>
               <ul class="navMiddle">
                     <li><a href="../html/index.html">News</a></li>
                     <li><a href="../html/dashboard.html">Dashboard</a></li>
-                    <li><a href="javascript:void(0)" id="subNavForm">Formulare</a></li>
+                    <li class="subNav">
+                        <a href="javascript:void(0)" id="subNavTitel">Formulare</a>
+                        <ul class="subNavElements">
+                            <li class="visibility">
+                                <a href="../html/formularurlaub.html" id="subNavUrlaub">Urlaub</a>
+                            </li>
+                            <li class="visibility">
+                                <a href="../html/formularfeedback.html" id="subNavFeed">Feedback</a>
+                            </li>
+                        </ul>
+                    </li>
                     <li><a href="./department.html">Abteilung</a></li>
               </ul>
-              <div class="navRight">
-                  <a href="../html/login.html" id="navLogin">Login</a>
-              </div>
+              <ul class="navRight">
+                <li>
+                    <a href="../html/login.html" id="navLogin">Login</a>
+                </li>
+              </ul>
           </nav>
-          <div class="subNav">
-                <a href="../html/formularurlaub.html" id="subNavUrlaub" class="visibility">Urlaub</a>
-                <a href="../html/formularfeedback.html" id="subNavFeed" class="visibility">Feedback</a>
-            </div>
       </header>
       <script type="text/javascript" src="../js/scriptDepartment.js"></script>
       `;
   }
-} 
+}
 
 customElements.define('header-component', HeaderComponent);
