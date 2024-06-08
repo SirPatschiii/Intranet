65ad48e3c770bbe15d4634e67da6f9389ac4f997
grilldb97 <dustin.bellem@web.de>
2024-06-08 12:23:30 +0200

    js für Urlaubsantrag ergänzt

--- src/js/scriptFormularUrlaub.js
+++ src/js/scriptFormularUrlaub.js
@@ -1,6 +1,33 @@
-const myForm = document.getElementById("urlaubsantrag");
-myForm.onsubmit = function(event) {
-    // Deine Aktionen hier
-    alert("Dein Urlaubsantrag wurde erfolgreich versendet!");
-    event.preventDefault(); // Verhindert das Standardverhalten des Formulars
+const FormUrlaub = document.getElementById("urlaubsantrag");
+FormUrlaub.onsubmit = function (event) {
+  event.preventDefault(); // Prevent the default form submission
+
+  const name = document.getElementById("name").value;
+  const von = document.getElementById("von").value;
+  const bis = document.getElementById("bis").value;
+  const vertretung = document.getElementById("vertretung").value;
+  const bemerkung = document.getElementById("urlaubbemerkung").value;
+
+  alert(
+    "Der Urlaubsantrag wurde erfolgreich abgesendet:" +
+      "\n" +
+      "Name: " +
+      name +
+      "\n" +
+      "Von: " +
+      von +
+      "\n" +
+      "Bis: " +
+      bis +
+      "\n" +
+      "Vertretung: " +
+      vertretung +
+      "\n" +
+      "Bemerkung: " +
+      bemerkung
+  );
+
+  window.location.href = "../html/index.html";
 };
+
+
